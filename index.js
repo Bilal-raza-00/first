e63e8ce389173ef840be1cba5651e65d6fedca16
Bilal-raza-00 <zulfiqar2233ali.com>
2024-05-25 02:42:50 +0500

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+// let i = 1;
+// while (1 <= 5){
+//     console .log ("1=",i);
+//     i++;
+// }
+
+
+//while loop
+
+let i= 1;
+while(i <= 10){
+    console .log ("i=", i)
+    i++;
+}
+
+//for of loop
+ 
+let str = "javaScript";
+let size = 0;
+for  (let val of str) {
+    console .log ("val=", val)
+    size++;
+}
+
+console . log ("string size", size)
